dbfe0354620384d970a395a2cc67934156713461
hakanaydins <auratheseason@gmail.com>
2022-10-07 01:09:58 +0300

    7 ekim

--- src/Hakan.java
+++ src/Hakan.java
@@ -1,5 +1,8 @@
 public class Hakan {
     public static void main(String[] args) {
         System.out.println("merhaba arkadaslar");
+
+        System.out.println("çok şükür hallettik");
+
     }
 }
